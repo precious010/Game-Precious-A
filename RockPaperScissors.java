3617bc9d33cb2a4d37ab37f20bddef9f68398eb6
PRECIOUS AJILORE <86179834+precious010@users.noreply.github.com>
2021-10-21 14:45:16 +0000

    I forgot to import for the action listener and action event as well as the Jframe, jLabel libraries

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,3 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.*;
+
+
+
+
+
 public class RockPaperScissors implements ActionListener{
 
 
